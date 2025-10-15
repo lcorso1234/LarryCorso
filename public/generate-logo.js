bbce7ef652b681b89a39220d33165a749b39f393
lcorso1234 <lawrencecorso1@gmail.com>
2025-10-14 20:36:25 -0500

    update to Batman-style logo with gold background

--- public/generate-logo.js
+++ public/generate-logo.js
@@ -1,16 +1,36 @@
 const logo = `
 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
-  <!-- Bat symbol logo -->
-  <g transform="translate(50, 50) scale(0.5)">
-    <path fill="#000000" d="M -80,-20 Q -90,-40 -95,-30 Q -100,-20 -90,-10 Q -85,0 -75,5 Q -65,8 -55,5 L -40,-5 Q -50,-15 -60,-20 Q -70,-22 -80,-20 Z"/>
-    <path fill="#000000" d="M 80,-20 Q 90,-40 95,-30 Q 100,-20 90,-10 Q 85,0 75,5 Q 65,8 55,5 L 40,-5 Q 50,-15 60,-20 Q 70,-22 80,-20 Z"/>
-    <ellipse fill="#000000" cx="0" cy="-15" rx="15" ry="20"/>
-    <path fill="#000000" d="M -40,-5 Q -45,10 -50,20 Q -55,25 -60,22 Q -58,15 -55,5 L -40,-5 Z"/>
-    <path fill="#000000" d="M 40,-5 Q 45,10 50,20 Q 55,25 60,22 Q 58,15 55,5 L 40,-5 Z"/>
-    <ellipse fill="#000000" cx="0" cy="10" rx="12" ry="18"/>
-    <path fill="#000000" d="M -8,-30 L -12,-40 L -5,-35 Z"/>
-    <path fill="#000000" d="M 8,-30 L 12,-40 L 5,-35 Z"/>
+  <!-- Batman-style logo -->
+  <defs>
+    <radialGradient id="bgGrad" cx="50%" cy="50%" r="50%">
+      <stop offset="0%" style="stop-color:#FFD700;stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#FFA500;stop-opacity:1" />
+    </radialGradient>
+  </defs>
+  
+  <circle cx="50" cy="50" r="48" fill="url(#bgGrad)"/>
+  
+  <g transform="translate(50, 52) scale(0.5)">
+    <path fill="#000000" d="
+      M 0,-50
+      L -8,-45 L -12,-48 L -18,-45 L -25,-38
+      Q -35,-28 -38,-20 L -42,-10 L -45,0 L -42,10 L -35,20
+      L -28,25 L -22,18 L -18,10 L -12,5 L -6,10 L -3,18
+      L 0,25
+      L 3,18 L 6,10 L 12,5 L 18,10 L 22,18 L 28,25
+      L 35,20 L 42,10 L 45,0 L 42,-10 L 38,-20
+      Q 35,-28 25,-38
+      L 18,-45 L 12,-48 L 8,-45
+      Z
+    "/>
+    <ellipse fill="#000000" cx="-25" cy="-5" rx="8" ry="12" transform="rotate(-20 -25 -5)"/>
+    <ellipse fill="#000000" cx="25" cy="-5" rx="8" ry="12" transform="rotate(20 25 -5)"/>
+    <ellipse fill="#000000" cx="0" cy="-20" rx="10" ry="15"/>
+    <path fill="#000000" d="M -6,-45 L -10,-55 L -4,-48 Z"/>
+    <path fill="#000000" d="M 6,-45 L 10,-55 L 4,-48 Z"/>
   </g>
+  
+  <circle cx="50" cy="50" r="48" fill="none" stroke="#000000" stroke-width="1"/>
 </svg>
 `;
 
